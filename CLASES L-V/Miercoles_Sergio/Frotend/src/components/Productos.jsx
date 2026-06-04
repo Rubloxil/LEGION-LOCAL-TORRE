7cf174117913736da470dd32718316f07083a203
RubieL Rodriguez <rubiel.tads@gmail.com>
2026-06-03 21:04:19 -0500

    add paginas module

---
+++ CLASES L-V/Miercoles_Sergio/Frotend/src/components/Productos.jsx
@@ -0,0 +1,15 @@
+function Productos() {
+  return (
+    <div>
+      <h2>Productos</h2>
+
+      <ul>
+        <li>GATO_MAN</li>
+        <li>Mouse</li>
+        <li>Teclado</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Productos;
