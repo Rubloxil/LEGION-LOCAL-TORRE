972210fd178179bff9c5118143952ea7aefba6a9
RubieL Rodriguez <rubiel.tads@gmail.com>
2026-06-03 19:34:43 -0500

    Entregable_2

---
+++ CLASES L-V/Miercoles_Sergio/Frotend/src/App.jsx
@@ -0,0 +1,13 @@
+import Carousel from "./components/Carousel";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Mi Carrusel Personalizado</h1>
+      <Carousel />
+    </>
+  );
+}
+
+export default App;
